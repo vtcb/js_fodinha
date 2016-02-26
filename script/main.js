ff31d28fb40e3db37ef2f007e9d761fddc67457b
Bolado <vtcb@cin.ufpe.br>
2016-02-26 00:19:26 -0300

    Add interface with the user
    Regularly update the screen with info.

--- script/main.js
+++ script/main.js
@@ -1,4 +1,14 @@
 function main() {
+    var IF = document.getElementById("interface");
+
+    setInterval(function() {
+        var info = "";
+        info += deck;
+        IF.innerHTML = info;
+    }, 100);
+
+
+    // Test Variables
     cards = [
         new Card(3, 0),
         new Card(0, 2),
@@ -9,4 +19,6 @@ function main() {
     deck = new Deck();
 
     deck.generate();
+
+    player = new Player();
 }
